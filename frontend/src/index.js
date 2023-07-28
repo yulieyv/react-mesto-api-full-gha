86f5cb702c268fd3795d79a398d1a936d75c78cb
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-28 21:32:56 +0300

    ПР15: попытка запустить фронт

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 reportWebVitals();
