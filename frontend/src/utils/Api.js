74ebd11359f206106f261d997c42e1a4be8a132c
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-28 20:31:24 +0300

    ПР15: добавлены файлы 3

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -84,9 +84,9 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "https://api.yamalieva.students.nomoredomains.xyz",
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-64",
   headers: {
-    authorization: `Bearer ${token}`,
+    authorization: "e761b675-104a-46b8-8d1b-b0499c848400",
     "Content-Type": "application/json",
   },
 });
