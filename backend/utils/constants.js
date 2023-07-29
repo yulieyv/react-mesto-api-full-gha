524b3d2a95985d9ecb2512803007bc9dd8411756
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-29 20:57:56 +0300

    ПР15: настройка обновления аватара

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,6 +1,6 @@
 const OK_STATUS = 200;
 const CREATED_STATUS = 201;
-const regexAvatar = /^https?:\/\/(?:[a-z0-9\\-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpe?g|gif|png|webp|bmp)$/im;
+const regexAvatar = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const regexLink = /^https?:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/im;
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
