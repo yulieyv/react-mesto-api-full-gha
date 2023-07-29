3b57829dcbb36b51b3fcaffdd24908c2a0c6eb18
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-29 17:09:05 +0300

    ПР15: исправляю ошибки

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -11,7 +11,6 @@ export function register(password, email) {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -25,7 +24,6 @@ export function login(password, email) {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -46,7 +44,6 @@ export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
